86ba31f3404e01d879a2527f9e9e07b962979fb8
Mateusz Podeszwa <podinatubie@gmail.com>
2024-12-22 05:59:19 +0000

    Create ActivityFactory.java

---
+++ CIS1068-N-BF1-2024/src/main/java/Application/DataPlanner/Activity/ActivityFactory.java
@@ -0,0 +1,12 @@
+package Application.DataPlanner.Activity;
+
+public class ActivityFactory {
+    /*
+    When you want a new activity instance, you ask the ActivityFactory.
+    The factory is configured at startup
+    (possibly by reading a configuration file or registry)
+    and knows which Activity class corresponds to each activity code.
+    * */
+
+
+}
