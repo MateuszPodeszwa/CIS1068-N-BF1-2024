908af095921a7fa1a2580a2078686017fc15fa23
Mateusz Podeszwa <podinatubie@gmail.com>
2025-01-04 14:30:51 +0000

    Create Functionality for ItineraryAddon

--- CIS1068-N-BF1-2024/src/main/java/Application/DataPlanner/itinerary/ItineraryAddon.java
+++ CIS1068-N-BF1-2024/src/main/java/Application/DataPlanner/itinerary/ItineraryAddon.java
@@ -1,4 +1,18 @@
 package Application.DataPlanner.itinerary;
 
-public class ItineraryAddon {
+public enum ItineraryAddon {
+
+    ACCOMMODATION(450_00),
+    LUNCH(10_00),
+    COFFEE(5_00);
+
+    private final int price;
+
+    ItineraryAddon(int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
 }
