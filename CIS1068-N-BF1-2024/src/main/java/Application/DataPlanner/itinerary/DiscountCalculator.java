d5df7547812ccef4195e88543afa6a3ff4d81693
Mateusz Podeszwa <podinatubie@gmail.com>
2025-01-04 14:31:35 +0000

    Make Discount Calculations work

--- CIS1068-N-BF1-2024/src/main/java/Application/DataPlanner/itinerary/DiscountCalculator.java
+++ CIS1068-N-BF1-2024/src/main/java/Application/DataPlanner/itinerary/DiscountCalculator.java
@@ -1,4 +1,36 @@
 package Application.DataPlanner.itinerary;
 
 public class DiscountCalculator {
+
+    private final int noOfActivities;
+    private final int noOfAttendees;
+
+    public DiscountCalculator(int noOfActivities, int noOfAttendees) {
+        if (noOfActivities < 0 || noOfAttendees < 0)
+            throw new IllegalArgumentException("Number of activities and attendees must be positive");
+
+        this.noOfActivities = noOfActivities;
+        this.noOfAttendees = noOfAttendees;
+    }
+
+    // Returns the percentage of a discount to be applied
+    public int calculateDiscount() {
+        switch (noOfAttendees) {
+            case 1: case 2:
+                if /**/ (noOfActivities < 10) return 0;
+                else if (noOfActivities < 20) return 5;
+                else /*                    */ return 8;
+
+            case 3: case 4: case 5:
+                if /**/ (noOfActivities < 10) return 5;
+                else if (noOfActivities < 20) return 8;
+                else /*                    */ return 12;
+
+            default:
+                if /**/ (noOfActivities < 10) return 10;
+                else if (noOfActivities < 20) return 12;
+                else /*                    */ return 14;
+        }
+    }
+
 }
